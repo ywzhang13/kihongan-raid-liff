c9ed5ffa6ce88b7678444076f5ee4023d91ed1ab
Jay <ywzhang0913@gmail.com>
2026-02-06 04:10:06 +0800

    Allow LINE webhook endpoint in SecurityConfig

--- src/main/java/com/kihongan/raidsystem/config/SecurityConfig.java
+++ src/main/java/com/kihongan/raidsystem/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         // Public endpoints
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers("/line/webhook").permitAll()
                         .requestMatchers("/raids").permitAll()
                         .requestMatchers("/raids/*/signups").permitAll()
                         
